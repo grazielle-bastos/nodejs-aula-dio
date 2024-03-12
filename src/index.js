c3d2b645b2bd76ddba2c71d853d39cc3f7147b93
Grazielle Bastos <grazielle1101@gmail.com>
2024-03-12 19:33:50 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+router.get("/contato", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(router)
+
+app.listen(3333, ()=>{
+    console.log("servidor rodando")
+})
+
+
+
+
+
+
+
+
+/*Criação de servidor local, sem a biblioteca express:
+const http = require("http")
+
+const hostname = "localhost"
+const port = 3333
+
+const server = http.createServer((req, res)=>{
+    res.setHeader("Content-type", "text/plain")
+    res.end("hello world")
+})
+
+server.listen(port, hostname, ()=>{
+    console.log("SERVIDOR RODANDO")
+})
+*/
