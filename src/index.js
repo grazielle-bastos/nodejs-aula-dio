f489bc346a13e6c151cde3c78f8e76c584610205
Grazielle Bastos <grazielle1101@gmail.com>
2024-03-13 23:10:23 -0300

    Renomeando página principal

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const router = express.Router()
 const pagesPath = path.join(__dirname, "pages")
 
 router.get("/", (req, res)=>{
-    res.sendFile(path.join(pagesPath, "home.html"))
+    res.sendFile(path.join(pagesPath, "index.html"))
 })
 
 router.get("/contato", (req, res)=>{
